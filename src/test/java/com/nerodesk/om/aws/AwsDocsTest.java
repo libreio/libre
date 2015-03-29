320e1f20ffe33ae2541a6e6f9faf3d19301dc2ab
longtimeago <lngtmg@gmail.com>
2015-03-29 19:29:25 +0300

    #159 Fix ordering issue in AwsDocsTest.listDocs()

--- src/test/java/com/nerodesk/om/aws/AwsDocsTest.java
+++ src/test/java/com/nerodesk/om/aws/AwsDocsTest.java
@@ -40,6 +40,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import nl.jqno.equalsverifier.EqualsVerifier;
@@ -116,6 +117,7 @@ public final class AwsDocsTest {
         final Bucket bucket = this.mockBucket(label, name);
         final List<String> expected = Arrays.asList(name, "sub/file");
         final List<String> names = new AwsDocs(bucket, label).names();
+        Collections.sort(names);
         MatcherAssert.assertThat(names, Matchers.equalTo(expected));
     }
 
