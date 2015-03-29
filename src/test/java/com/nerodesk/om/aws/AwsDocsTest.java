9a5c56af146513efcea3cc0bb871d7436dd37eb9
longtimeago <lngtmg@gmail.com>
2015-03-29 19:33:19 +0300

    #159 Checkstyle is happy

--- src/test/java/com/nerodesk/om/aws/AwsDocsTest.java
+++ src/test/java/com/nerodesk/om/aws/AwsDocsTest.java
@@ -40,7 +40,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import nl.jqno.equalsverifier.EqualsVerifier;
